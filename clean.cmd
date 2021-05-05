9851949a3db3578b17123bc8bbb70fc82a886551
Windowsist <Windowsist@live.com>
2021-05-05 12:05:57 +0800

    Delete clean.cmd

--- clean.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d C:\Users\Windowsist\_vscode
-for /d %%i in (*) do  if exist "%%i\build" rd /s /q "%%i\build"
-cd /d C:\Users\Windowsist\AppData\Roaming\Code
-for /d %%i in (Cache CachedData CachedExtensions "Code Cache"  GPUCache logs User\workspaceStorage) do  if exist %%i rd /s /q %%i
-for /d %%i in (Cookies Cookies-journal) do  if exist %%i del %%i
-if exist C:\Users\Windowsist\AppData\Local\CMakeTools\log.txt del C:\Users\Windowsist\AppData\Local\CMakeTools\log.txt
-echo cleaned
-pause
